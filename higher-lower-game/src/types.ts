1a2b12dbd5f140670847e4e6cb99b07f168e9158
aalessio01 <alessio.armenio@axelspringer.com>
2026-01-16 11:45:54 +0100

    fix deployment

--- higher-lower-game/src/types.ts
+++
@@ -1,6 +0,0 @@
-export type Question = {
-  id: number;
-  question: string;
-  options: { text: string; isCorrect: boolean; imageUrl: string; }[];
-  feedback: { correct: string; incorrect: string;}
-};
